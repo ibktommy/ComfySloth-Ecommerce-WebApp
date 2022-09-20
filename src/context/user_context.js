62ea86d1dadb04fb888c18e37becd571e5da7704
ibktommy <atomquartz@gmail.com>
2022-09-20 11:06:39 +0100

    Comment out some imports of dependency that is not yet installed

--- src/context/user_context.js
+++ src/context/user_context.js
@@ -1,5 +1,5 @@
 import React, { useContext, useEffect, useState } from 'react'
-import { useAuth0 } from '@auth0/auth0-react'
+// import { useAuth0 } from '@auth0/auth0-react'
 
 const UserContext = React.createContext()
 export const UserProvider = ({ children }) => {
