496d1c4cd62a74661640c98ddeadf77e9f6591f8
ibktommy <atomquartz@gmail.com>
2022-09-20 11:44:03 +0100

    Setting up the Routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { Navbar, Sidebar, Footer } from './components'
+import { Home, SingleProduct, Cart, Error, PrivateRoute, About, Products, Checkout } from './pages'
 
 function App() {
-  return <h4>comfy sloth starter</h4>
+  return (
+    <Router>
+      <Navbar />
+      <Sidebar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/cart' element={<Cart />} />
+        <Route path='/product' element={<Products />} />
+        <Route path='/product/:id' element={<SingleProduct />} />
+        <Route path='/checkout' element={<Checkout />} />
+        <Route path='*' element={<Error />} />
+      </Routes>
+      <Footer />
+    </Router>
+  )
 }
 
 export default App
