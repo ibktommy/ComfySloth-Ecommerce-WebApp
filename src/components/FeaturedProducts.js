4f0118cf8fb2980e0a0072c610965b5987c93c27
ibktommy <atomquartz@gmail.com>
2022-09-21 14:01:19 +0100

    Create function that is used to format the price of products so as to make it better dynamic and formatted

--- src/components/FeaturedProducts.js
+++ src/components/FeaturedProducts.js
@@ -30,7 +30,7 @@ const FeaturedProducts = () => {
         <div className="underline"></div>
       </div>
       <div className="section-center featured">
-        {featuredProducts.map((product) => {
+        {featuredProducts.slice(0, 3).map((product) => {
           return (
             <Product key={product.id} {...product}/>
           )
--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,3 +1,10 @@
-export const formatPrice = () => {}
+export const formatPrice = (number) => {
+  const formattedNumber = Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+  }).format(number / 100)
+
+  return formattedNumber
+}
 
 export const getUniqueValues = () => {}
