d62cc3e26c900171019d13852717209b5eb491ac
ibktommy <atomquartz@gmail.com>
2022-09-22 15:40:10 +0100

    Add elements to the Star Component

--- src/components/Stars.js
+++ src/components/Stars.js
@@ -1,8 +1,14 @@
 import React from 'react'
 import styled from 'styled-components'
 import { BsStarFill, BsStarHalf, BsStar } from 'react-icons/bs'
-const Stars = () => {
-  return <h4>stars</h4>
+const Stars = ({ stars, reviews }) => {
+
+  return (
+    <Wrapper>
+      <div className="stars">Stars</div>
+      <p className="reviews">{reviews}</p>
+    </Wrapper>
+  )
 }
 
 const Wrapper = styled.div`
