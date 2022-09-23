61104a00e593e26053342f9098341c09dded0fce
ibktommy <atomquartz@gmail.com>
2022-09-23 17:16:48 +0100

    Add elements to the AmountButtons component

--- src/components/AmountButtons.js
+++ src/components/AmountButtons.js
@@ -2,8 +2,19 @@ import React from 'react'
 import styled from 'styled-components'
 import { FaPlus, FaMinus } from 'react-icons/fa'
 
-const AmountButtons = () => {
-  return <h4>amount buttons </h4>
+const AmountButtons = ({ amount, increase, decrease }) => {
+
+  return (
+    <Wrapper className='amount-btns'>
+      <button className="amount-btn" onClick={() => increase}>
+        <FaMinus />
+      </button>
+      <h2 className="amount">{amount}</h2>
+      <button className="amount-btn" onClick={() => decrease}>
+        <FaPlus />
+      </button>
+    </Wrapper>
+  )
 }
 
 const Wrapper = styled.div`
