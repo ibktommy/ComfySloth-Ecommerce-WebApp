c9033783df8f484b734d68a1cd1ab50384b747ab
ibktommy <atomquartz@gmail.com>
2022-09-24 11:57:59 +0100

    Wrapping the FilteredProvider with the ProductProvider so that it can have access to the values in the ProductProvider

--- src/index.js
+++ src/index.js
@@ -11,7 +11,9 @@ import { UserProvider } from './context/user_context'
 ReactDOM.render
   (
     <ProductsProvider>
-      <App />
+      <FilterProvider>
+        <App />
+      </FilterProvider>
     </ProductsProvider>,
     document.getElementById('root')
   )
