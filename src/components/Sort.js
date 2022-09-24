87fd34e8cd4f082fda1a28e1abf9384cb2bedf46
ibktommy <atomquartz@gmail.com>
2022-09-24 13:45:42 +0100

    Working on the Sort Component - conditionally setting class properties to the buttons based on the value of the gridView-state

--- src/components/Sort.js
+++ src/components/Sort.js
@@ -2,8 +2,22 @@ import React from 'react'
 import { useFilterContext } from '../context/filter_context'
 import { BsFillGridFill, BsList } from 'react-icons/bs'
 import styled from 'styled-components'
+
 const Sort = () => {
-  return <h4>sort </h4>
+  const { filtered_products, gridView } = useFilterContext()
+
+  return (
+    <Wrapper>
+      <div className="btn-container">
+        <button type='button' className={`${gridView ? 'active' : null}`}>
+          <BsFillGridFill />
+        </button>
+        <button type='button' className={`${!gridView ? 'active' : null}`}>
+          <BsList />
+        </button>
+      </div>
+    </Wrapper>
+  )
 }
 
 const Wrapper = styled.section`
