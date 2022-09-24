6aa57a84eee86f6664d4f91cec40cb527a6610b1
ibktommy <atomquartz@gmail.com>
2022-09-24 13:54:00 +0100

    Adding elements and form to the Sort Component

--- src/components/Sort.js
+++ src/components/Sort.js
@@ -4,7 +4,7 @@ import { BsFillGridFill, BsList } from 'react-icons/bs'
 import styled from 'styled-components'
 
 const Sort = () => {
-  const { filtered_products, gridView } = useFilterContext()
+  const { filtered_products: products, gridView } = useFilterContext()
 
   return (
     <Wrapper>
@@ -16,6 +16,18 @@ const Sort = () => {
           <BsList />
         </button>
       </div>
+
+      <p>{products.length} products found</p>
+      <hr />
+      <form>
+        <label htmlFor="sort">sort by</label>
+        <select name="sort" id="sort" className='sort-input'>
+          <option value="price-lowest">price (lowest)</option>
+          <option value="price-highest">price (highest)</option>
+          <option value="name-a">name (a-z)</option>
+          <option value="name-z">name (z-a)</option>
+        </select>
+      </form>
     </Wrapper>
   )
 }
