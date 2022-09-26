0a2930bc3df22338395b7347e98f4093585ac1e3
ibktommy <atomquartz@gmail.com>
2022-09-26 11:55:40 +0100

    Write functionality for the sort-select-options

--- src/reducers/filter_reducer.js
+++ src/reducers/filter_reducer.js
@@ -41,9 +41,28 @@ const filter_reducer = (state, action) => {
   }
 
   if (action.type === SORT_PRODUCTS) {
-    console.log(state.sort)
+    const { sort, filtered_products } = state
+    let sortedProducts = []
+
+    if (sort === 'all') {
+      sortedProducts = filtered_products
+    }
+    if (sort === 'price-highest') {
+      sortedProducts = filtered_products.sort((a, b) => b.price - a.price)
+    }
+    if (sort === 'price-lowest') {
+      sortedProducts = filtered_products.sort((a, b) => a.price - b.price)
+    }
+    if (sort === 'name-a') {
+      sortedProducts = filtered_products.sort((a, b) => a.name.localeCompare(b.name))
+    }
+    if (sort === 'name-z') {
+      sortedProducts = filtered_products.sort((a, b) => b.name.localeCompare(a.name))
+    }
+
     return {
       ...state,
+      filtered_products: sortedProducts,
     }
   }
   throw new Error(`No Matching "${action.type}" - action type`)
